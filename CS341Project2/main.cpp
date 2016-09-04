1c6633bb7e6eae3e098dd85c36f1f6ffff3208a4
Daniel Pulley <dpulley@uic.edu>
2016-09-03 22:02:08 -0500

    Add main function and preprocessor directives

---
+++ CS341Project2/main.cpp
@@ -0,0 +1,34 @@
+//
+// Netflix Movie Review Analysis
+//
+// <<Daniel Pulley>>
+// U. of Illinois, Chicago
+// CS341, Fall 2016
+// HW #02
+//
+
+// Preprocess Directives
+#include <iostream>
+#include <iterator>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+// using namespace std
+using std::string;
+using std::ifstream;
+using std::vector;
+using std::stringstream;
+using std::getline;
+using std::stoi;
+using std::sort;
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+}
