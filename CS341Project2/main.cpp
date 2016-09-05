b194e273b757c2baf131607f66adfe439c0a46fa
Daniel Pulley <dpulley@uic.edu>
2016-09-05 18:41:16 -0500

    Create input functions for both movies and reviews
    both functions take a reference to a vector as a parameter and insert data
    into the vectors respectively

--- CS341Project2/main.cpp
+++ CS341Project2/main.cpp
@@ -16,6 +16,8 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include "Movie.h"
+#include "Review.h"
 
 // using namespace std
 using std::string;
@@ -28,12 +30,113 @@ using std::sort;
 using std::cout;
 using std::endl;
 
+/*
+ * inputMovies function
+ * 
+ * Function takes movie vector and then inputs data from movies CSV file into the vector
+ */
+void inputMovies(vector<Movie>& myMovie)
+{
+	// Open movie.csv file
+	ifstream movie_file("movies.csv");
 
-// Main Function
+
+	// String values to get Movie statistics
+	string line, movie_id, movie_name, publish_year;
+
+	// Check if movies file exist, if not exit program
+	if (!movie_file.good()) {
+		cout << "Cannot open movie file!" << endl;
+		cout << "Exiting Program!" << endl;
+		return;
+	}
+
+	// Skip header
+	getline(movie_file, line);
+
+	while (getline(movie_file, line)) {
+
+		// Use stringstream to help parse CSV file
+		stringstream ss(line);
+
+		// parse line:
+		getline(ss, movie_id, ',');
+		getline(ss, movie_name, ',');
+		getline(ss, publish_year);
+
+		Movie M(stoi(movie_id), movie_name, stoi(publish_year));
+
+		// insert @ end
+		myMovie.push_back(M);
+	}
+}
+/* End of inputMovies File */
+
+/*
+* inputReviews function
+*
+* Function takes review vector and then inputs data from reviews CSV file into the vector
+*/
+void inputReviews(vector<Review>& myReview)
+{
+	// Open Review CSV file
+	ifstream review_file("reviews.csv");
+
+
+	// String values to get Movie statistics
+	string line, movie_id, user_id, movie_rating, review_date;
+
+	// Check if movies file exist, if not exit program
+	if (!review_file.good()) {
+		cout << "Cannot open review file!" << endl;
+		cout << "Exiting Program!" << endl;
+		return;
+	}
+
+	// Skip header
+	getline(review_file, line);
+
+	while (getline(review_file, line)) {
+
+		// Use stringstream to help parse CSV file
+		stringstream ss(line);
+
+		// parse line:
+		getline(ss, movie_id, ',');
+		getline(ss, user_id, ',');
+		getline(ss, movie_rating, ',');
+		getline(ss, review_date);
+
+		Review R(stoi(movie_id), stoi(user_id), stoi(movie_rating), review_date);
+
+		// insert @ end
+		myReview.push_back(R);
+	}
+}
+/* End of inputReviews Function*/
+
+
+/* 
+ * main Function
+ *
+ */
 int main()
 {
 	// Print Title of Program
 	cout << "** Netflix Movie Review Analysis **" << endl;
+	
+	// Create Movie vector to input movie data from file
+	vector<Movie> movies;
+	
+	// Create Review vector to input review data from file
+	vector<Review> reviews;
+
+	// Input movies into vector
+	inputMovies(movies);
+
+	// Input reviews into vector
+	inputReviews(reviews);
+
 	system("PAUSE");
 }
-// End of Main Function
+/* End of Main Function */
